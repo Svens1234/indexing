33d3a801e987d17402681cdbb154fc872a0b6b5b
Svenden123 <buhrus1@yandex.ru>
2021-07-29 21:39:04 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+greeting = 'Hello Python!'
+greeting_length = len(greeting)
+print(len(greeting))
+
+print(greeting[1])
+print(greeting[6])
+print(greeting[-1])
+print(greeting[12])
+print(greeting[-4])
+
+
+
